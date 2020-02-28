c2344dc33faa294b010da027aeeba51686d9b07e
harsh <harshsingh9613@gmail.com>
2020-02-28 17:07:41 +0530

    Created Signup Form and Header and add reactstrap library

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+import Header from './Components/Header';
+import SignUp from './Components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <SignUp />
     </div>
   );
 }
